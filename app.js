15c5ba57b9c0b649a54c5647722ce25ea10df5c8
Lin <mt5718214@gmail.com>
2020-11-23 16:14:01 +0800

    Add userController and signup page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 const port = 3000
 
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   db.sequelize.sync()  //讓models與資料庫同步
