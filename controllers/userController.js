e2b5d4f1a1873aacb2928781b8b3741a64690f6d
Lin <mt5718214@gmail.com>
2020-11-24 09:58:21 +0800

    Add navbar to show login status

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,12 +37,12 @@ const userController = {
   },
 
   signIn: (req, res) => {
-    req.flash('success_messages', '成功登入！')
+    req.flash('success_msg', '成功登入！')
     res.redirect('/restaurants')
   },
 
   logout: (req, res) => {
-    req.flash('success_messages', '登出成功！')
+    req.flash('success_msg', '登出成功！')
     req.logout()
     res.redirect('/signin')
   }
