d58e9d6cfa78163f6aa4876135a882516af6f801
Lin <mt5718214@gmail.com>
2020-11-24 16:07:48 +0800

    Add admin restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -10,6 +10,24 @@ const adminController = {
 
   createRestaurant: (req, res) => {
     return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_msg', "name didn't exist")
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description
+    })
+      .then((restaurant) => {
+        req.flash('success_msg', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
 
