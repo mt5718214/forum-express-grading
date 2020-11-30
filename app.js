5e881eed3714b6f6241ee075e8eea6e459f58a03
Lin <mt5718214@gmail.com>
2020-11-30 20:34:18 +0800

    Add selected option condition to category select

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/handlebars-helpers') }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({
