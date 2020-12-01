205fddc6a580a73c07b188960d020c1ee34e403b
Lin <mt5718214@gmail.com>
2020-12-01 17:47:04 +0800

    add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,17 +2,35 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let categoryId = ''
+
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
     }
 
-    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
-      const data = restaurants.map(r => ({
+    Restaurant.findAndCountAll({
+      include: Category,
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit) //findAndCountAll中可利用count取得資料筆數
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      const data = result.rows.map(r => ({  // findAndCountAll中可利用rows取得資料
         ...r.dataValues,  //利用展開運算子複製物件並展開其內容
         description: r.dataValues.description.substring(0, 50),
         categoryName: r.Category.name
@@ -22,7 +40,11 @@ const restController = {
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          totalPage,
+          prev,
+          next,
+          page
         })
       })
     })
