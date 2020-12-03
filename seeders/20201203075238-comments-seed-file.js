f6676626775981229c71a9f6c7e50ee5245ab4bb
Lin <mt5718214@gmail.com>
2020-12-03 17:10:06 +0800

    modify comment-seed-file

--- seeders/20201203075238-comments-seed-file.js
+++ seeders/20201203075238-comments-seed-file.js
@@ -1,13 +1,20 @@
 'use strict';
+const db = require('../models')
+const User = db.User
+const Restaurant = db.Restaurant
+
 const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const users = await User.findAll({ raw: true })
+    const restaurants = await Restaurant.findAll({ raw: true })
+
     await queryInterface.bulkInsert('Comments',
       Array.from({ length: 3 }).map((_, index) => ({
         text: faker.lorem.text(),
-        UserId: 1,
-        RestaurantId: index * 10 + 1,
+        UserId: users[Math.floor(Math.random() * users.length)].id,
+        RestaurantId: restaurants[Math.floor(Math.random() * users.length)].id,
         createdAt: new Date(),
         updatedAt: new Date()
       }))
